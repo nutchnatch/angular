d69d06af3fb8e3aadeebb219d036aed6049ee6f8
nivaldomonteiro <nushnivaldo@gmail.com>
2020-12-14 22:36:54 +0000

    Added observables completion, operators and subjects - closes #30

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -33,7 +34,11 @@ export class HomeComponent implements OnInit {
         }, 1000);
     });
 
-    this.intervalSubscribtion = constantIntervalObservable.subscribe(data => {
+    this.intervalSubscribtion = constantIntervalObservable.pipe(filter(data => {
+      return data > 0;
+    }), map((data: number) =>  {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
         console.log(data);
       }, error2 => {
         console.log(error2);
