dc0ac5d7a571579eb298b465f69dc50f17e48cb2
nivaldomonteiro <nushnivaldo@gmail.com>
2020-12-29 00:37:16 +0000

    Added http calls to persist data - closes #45

--- course-prj/src/app/header/header.component.ts
+++ course-prj/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage';
 
 @Component ({
     selector: 'app-header',
@@ -6,5 +7,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-    
+    constructor(private dataStorageService: DataStorageService) {}
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes();
+    }
+
+    onFectData() {
+        this.dataStorageService.fetchRecipes()
+            .subscribe();
+    }
 }
---
+++ course-prj/src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,21 @@
+import  { Injectable } from '@angular/core';
+import { ActivatedRoute, ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Recipe } from './recipe.model';
+import { DataStorageService } from '../shared/data-storage';
+import { RecipeService } from './recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+    constructor(private dataStorageSerice: DataStorageService,
+                private recipeService: RecipeService){};
+    
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const recipes = this.recipeService.getRecipes();
+        if(recipes.length === 0) {
+            return this.dataStorageSerice.fetchRecipes();
+        } else {
+            return recipes;
+        }
+        
+    }
+}
