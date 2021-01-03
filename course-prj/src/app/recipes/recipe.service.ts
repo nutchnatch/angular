9b6904eb80928bb1899edbf99b3f52804d069603
nivaldomonteiro <nushnivaldo@gmail.com>
2021-01-03 02:49:35 +0000

    Added only one root state - closes #71

--- course-prj/src/app/recipes/recipe.service.ts
+++ course-prj/src/app/recipes/recipe.service.ts
@@ -4,13 +4,13 @@ import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 import { Store } from '@ngrx/store';
 import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
-import * as fromShoppingList from '../shopping-list/store/shopping-list.reducer';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
     constructor(
-        private store: Store<fromShoppingList.AppState>
+        private store: Store<fromApp.AppState>
       ) {}
 
     // private recipes: Recipe[] = [
